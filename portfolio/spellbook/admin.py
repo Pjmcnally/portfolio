f45c6b60666caab7cb73b055103e0873bd59731b
Patrick McNally <Patrick.J.McNally@gmail.com>
2016-12-15 19:06:55 -0600

    add all spellbook models to admin

--- portfolio/spellbook/admin.py
+++ portfolio/spellbook/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import (CastingTime, Clss, Component, Domain, Duration, Level,
+                     Range, School, Source, SpellSource, SubDomain, Spell)
 
 # Register your models here.
+admin.site.register(CastingTime)
+admin.site.register(Clss)
+admin.site.register(Component)
+admin.site.register(Domain)
+admin.site.register(Duration)
+admin.site.register(Level)
+admin.site.register(Range)
+admin.site.register(School)
+admin.site.register(Source)
+admin.site.register(SpellSource)
+admin.site.register(SubDomain)
+admin.site.register(Spell)
