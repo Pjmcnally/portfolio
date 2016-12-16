21fffbd65dcd6de39d8201cabd6fa36bd3f22b09
Patrick McNally <Patrick.J.McNally@gmail.com>
2016-12-15 18:49:25 -0600

    fix misspelling in url

--- portfolio/spellbook/urls.py
+++ portfolio/spellbook/urls.py
@@ -5,7 +5,7 @@ from .views import spell_detail, spell_page, spells
 urlpatterns = [
     url(r'^$', spell_page, name='sb_spell_page'),
     url(r'^spells$', spells, name='sb_spells'),
-    url(r'^spell/(?P<slug>[\w-]+)/$', spell_detail, name='sb_pell_detail'),
+    url(r'^spell/(?P<slug>[\w-]+)/$', spell_detail, name='sb_spell_detail'),
     # Never called but needed or the links in the main nav will break the site.
     url(r'^class/(?P<slug>[\w-]+)/$', spell_page, name='class_spell_list'),
 ]
