c47d6d2ad31fc1d5c9e96181bc8d27c84fefb950
Patrick McNally <Patrick.J.McNally@gmail.com>
2016-12-15 18:57:43 -0600

    Rename spell_list_page back to spell_list for brevity

--- portfolio/spellbook/urls.py
+++ portfolio/spellbook/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import url
 
-from .views import spell_detail, spell_list_page, spells
+from .views import spell_detail, spell_list, spells
 
 urlpatterns = [
-    url(r'^$', spell_list_page, name='sb_spell_list_page'),
+    url(r'^$', spell_list, name='sb_spell_list'),
     url(r'^spells$', spells, name='sb_spells'),
     url(r'^spell/(?P<slug>[\w-]+)/$', spell_detail, name='sb_spell_detail'),
     # Never called but needed or the links in the main nav will break the site.
-    url(r'^class/(?P<slug>[\w-]+)/$', spell_list_page, name='sb_class_spell_list'),
+    url(r'^class/(?P<slug>[\w-]+)/$', spell_list, name='sb_class_spell_list'),
 ]
