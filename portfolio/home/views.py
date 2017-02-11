a9b8de843206caaac174b60360ed84085249e907
Patrick McNally <Patrick.J.McNally@gmail.com>
2017-02-11 17:25:00 -0600

    Add new view for content on main page

--- portfolio/home/views.py
+++ portfolio/home/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import ensure_csrf_cookie
 
 
-# Create your views here.
 def home(request):
     return render(request, 'home/base.html', {})
+
+
+@ensure_csrf_cookie
+def content(request):
+    if request.method == 'POST':
+        page = request.POST.get("page", None)
+
+    return render(request, "home{}.html".format(page))
