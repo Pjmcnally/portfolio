99d7322e110fee31baa5bbc22b9a29c643f68e60
Patrick McNally <Patrick.J.McNally@gmail.com>
2017-03-05 00:40:38 -0600

    Add new URLs for random and json

--- portfolio/spellbook/urls.py
+++ portfolio/spellbook/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 
-from .views import spell_detail, spell_list, spells
+from .views import spell_detail, spell_list, spells, random, json_random
 
 urlpatterns = [
     url(r'^$', spell_list, name='sb_spell_list'),
@@ -8,4 +8,6 @@ urlpatterns = [
     url(r'^spell/(?P<slug>[\w-]+)/$', spell_detail, name='sb_spell_detail'),
     # Never called but needed or the links in the main nav will break the site.
     url(r'^class/(?P<slug>[\w-]+)/$', spell_list, name='sb_class_spell_list'),
+    url(r'^random/$', random, name="sb_random"),
+    url(r'^json_random/$', json_random, name="sb_json_random"),
 ]
