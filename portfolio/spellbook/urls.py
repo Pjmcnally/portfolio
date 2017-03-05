5ff2fce0aa20a4b6d26dbb75ef89a53af0ba531c
Patrick McNally <Patrick.J.McNally@gmail.com>
2017-03-05 01:50:50 -0600

    rename random_json and use class based view

--- portfolio/spellbook/urls.py
+++ portfolio/spellbook/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 
-from .views import spell_detail, spell_list, spells, random, json_random
+from .views import spell_detail, spell_list, spells, random, RandomJson
 
 urlpatterns = [
     url(r'^$', spell_list, name='sb_spell_list'),
@@ -9,5 +9,5 @@ urlpatterns = [
     # Never called but needed or the links in the main nav will break the site.
     url(r'^class/(?P<slug>[\w-]+)/$', spell_list, name='sb_class_spell_list'),
     url(r'^random/$', random, name="sb_random"),
-    url(r'^json_random/$', json_random, name="sb_json_random"),
+    url(r'^random_json$', RandomJson.as_view(), name="sb_json_random"),
 ]
