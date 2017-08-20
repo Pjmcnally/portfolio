8b909ef6fe04c78bf51aad38240b16f9faaff474
Patrick McNally <Patrick.J.McNally@gmail.com>
2017-08-19 20:52:46 -0500

    Add players data file and write basic model for players

--- portfolio/baseball/models.py
+++ portfolio/baseball/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
+class player(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharFiled(max_length=50)
+    ret_code = models.charField(max_length=8, Unique=True)
+    # slug = models.SlugField(
+    #     max_length=50,
+    #     unique=True,
+    #     help_text='A label for URL config')
+
+    def __str__(self):
+        return "{} {}".format(self.first_name, self.last_name)
+
+    class Meta:
+        ordering = ['last_name']
