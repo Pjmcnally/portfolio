cef8975a83c871ae0876417d13efd8a4069ee217
Author Name <Patrick.J.McNally@gmail.com>
2018-03-08 15:45:40 -0600

    Fix issues with commas.

--- portfolio/spellbook/urls.py
+++ portfolio/spellbook/urls.py
@@ -10,10 +10,10 @@ urlpatterns = [
     url(r'^random/$', random, name="sb_random"),
 
     # Depreciated link after new class search. Redirecting to main spell list.
-    url(r'^class/\w*/$', spell_list_redirect, name='sb_class_spell_list')
+    url(r'^class/\w*/$', spell_list_redirect, name='sb_class_spell_list'),
 
     # Ajax request endpoints
     url(r'^spells$', spells, name='sb_spells'),
     url(r'^get_spell_detail$', get_spell_detail, name='sb_get_spell_detail'),
-    url(r'^random_json$', RandomJson.as_view(), name="sb_json_random"),
+    url(r'^random_json$', RandomJson.as_view(), name="sb_json_random")
 ]
