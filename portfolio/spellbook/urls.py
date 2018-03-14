19eca9e2c993cd2382a3ceaa4d6890f5c9a7dc36
Patrick McNally <Patrick@pjmcnally.net>
2018-03-13 23:56:50 -0500

    Simplify import.

--- portfolio/spellbook/urls.py
+++ portfolio/spellbook/urls.py
@@ -1,19 +1,18 @@
 from django.conf.urls import url
 
-from .views import (spell_detail, get_spell_detail, spell_list,
-    spell_list_redirect, spells, random, RandomJson)
+from . import views as v
 
 urlpatterns = [
     # Site pages
-    url(r'^$', spell_list, name='sb_spell_list'),
-    url(r'^spell/(?P<slug>[\w-]+)/$', spell_detail, name='sb_spell_detail'),
-    url(r'^random/$', random, name="sb_random"),
+    url(r'^$', v.spell_list, name='sb_spell_list'),
+    url(r'^spell/(?P<slug>[\w-]+)/$', v.spell_detail, name='sb_spell_detail'),
+    url(r'^random/$', v.random, name="sb_random"),
 
     # Depreciated link after new class search. Redirecting to main spell list.
-    url(r'^class/\w*/$', spell_list_redirect, name='sb_class_spell_list'),
+    url(r'^class/\w*/$', v.spell_list_redirect, name='sb_class_spell_list'),
 
     # Ajax request endpoints
-    url(r'^spells$', spells, name='sb_spells'),
-    url(r'^get_spell_detail$', get_spell_detail, name='sb_get_spell_detail'),
-    url(r'^random_json$', RandomJson.as_view(), name="sb_json_random")
+    url(r'^spells$', v.spells, name='sb_spells'),
+    url(r'^get_spell_detail$', v.get_spell_detail, name='sb_get_spell_detail'),
+    url(r'^random_json$', v.RandomJson.as_view(), name="sb_json_random")
 ]
